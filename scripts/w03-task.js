2837f53e67ca45ec018bd0882ee3a0681b52ffec
Uwa Joseph Uwa <uwauwajoe@gmail.com>
2024-01-31 03:54:36 +0100

    errors fixed

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,59 +1,61 @@
-/* LESSON 3 - Programming Tasks */
-
 /* FUNCTIONS */
-function add (number1, number2) {
+
+/* Function Definition - Add Numbers */
+function add(number1, number2) {
     return number1 + number2;
 }
-/* Function Definition - Add Numbers */
-
-let addNumber1 = Number (document.querySelector('#add1').value);
-document.querySelector('#sum').value = add (addNumber1, addNumber2);
-document.querrySelector('#addNumbers').addEventListener ('clicks', addNumbers);
+function addNumbers() {
+let addNumber1 = Number(document.querySelector('#add1').value);
+let addNumber2 = Number(document.querySelector('#add2').value);
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 /* Function Expression - Subtract Numbers */
-
-const Subtract = function (number1, number2) {
+const subtract = function(number1, number2) {
     return number1 - number2;
 };
 
-const SubtractNumbers = function () {
-    let subtractNumber1 = Number (document.querySelector('#subtract1').value);
-    let subtractNumber2 = Number (document.querrySelector('#substract2').value);
-    document.querySelector('#difference').value = substract (subtractNumber1, subtractNumber2);
+const subtractNumbers = function() {
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
 };
-document.querySelector ('subractNumbers').addEventListener ('clicks', subtractNumbers);
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
-const multiply = (number1, number2) => number1 * number2; 
+const multiply = (number1, number2) => number1 * number2;
 
 const multiplyNumbers = () => {
     let factor1 = Number(document.querySelector('#factor1').value);
     let factor2 = Number(document.querySelector('#factor2').value);
     let product = multiply(factor1, factor2);
     document.getElementById('product').value = product;
-}
+};
 document.getElementById('multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
-const divide = (number1, number2) => number1 / number2
+const divide = (number1, number2) => number1 / number2;
 
 const divideNumbers = () => {
     let dividend = parseFloat(document.querySelector('#dividend').value);
     let divisor = parseFloat(document.querySelector('#divisor').value);
     let quotient = divide(dividend, divisor);
     document.getElementById('quotient').value = quotient.toFixed(2);
-}
+};
 document.getElementById('divideNumbers').addEventListener('click', divideNumbers);
+
 /* Decision Structure */
-function gettotal() {
+function getTotal() {
     // Input
     let subtotal = parseFloat(document.querySelector('#subtotal').value);
     // Processing
+    let total;
     if (document.getElementById('member').checked) {
         total = subtotal * 0.8;
     } else {
         total = subtotal;
     }
-    document.getElementById('total').textContent = `$ ${subtotal.toFixed(2)}`
+    document.getElementById('total').textContent = `$ ${total.toFixed(2)}`;
 }
 
-document.getElementById('getTotal').addEventListener('click', gettotal)
+document.getElementById('getTotal').addEventListener('click', getTotal);

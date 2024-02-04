0027bed27e9bbcf2d758117c25fcab2e856dc080
Uwa Joseph Uwa <uwauwajoe@gmail.com>
2024-02-04 18:22:41 +0100

    corrections made

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,8 +1,6 @@
 /* W02-Task - Profile Home Page */
 
-
 /* Step 1 - Setup type tasks - no code required */
-
 
 /* Step 2 - Variables */
 const fullName = "Uwa Joseph Uwa";
@@ -11,42 +9,40 @@ const profilePicture = "images/uwajoe.jpg";
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const nameElement = document.querySelector("#name");
 const imageElement = document.querySelector("img");
-const foodElement = document.querySelector("#food")
+
 
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}<strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
-
-
-
-
+imageElement.setAttribute("alt", `profile picture of ${profilePicture}`);
 
 
 /* Step 5 - Array */
 let favFood = ["Beans", "White Soup", "Pizza", "Afang Soup", "Fried Rice", "Egusi Soup", "Banana", "Ice cream", "Strawberry"];
-foodElement.innerHTML = favFood.join('<br>'); // Display the array elements with line breaks
+let singlefavfood = "Watermelon";
+foodElement.textContent = favFood;
+favFood.push(singlefavFood);
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.splice(0, 1);
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
 
-// Add a new food to the array
-let newfood = "Watermelon";
-favFood.push(newfood);
-foodElement.innerHTML += `<br>${newfood}`;
+// // Add a new food to the array
+// let newfood = "Watermelon";
+// favFood.push(newfood);
+// foodElement.innerHTML += `<br>${newfood}`;
 
-// Remove the first and last elements
-const firstFood = favFood.shift();
-const lastFood = favFood.pop();
+// // Remove the first and last elements
+// const firstFood = favFood.shift();
+// const lastFood = favFood.pop();
 
-// Render the removed elements in addition to the previous array values (with a line break)
-foodElement.innerHTML += `<br>${firstFood}`;
-foodElement.innerHTML += `<br>${lastFood}`;
-
-
-
-
-
-
-
+// // Render the removed elements in addition to the previous array values (with a line break)
+// foodElement.innerHTML += `<br>${firstFood}`;
+// foodElement.innerHTML += `<br>${lastFood}`;

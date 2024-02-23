b996ad42335f141ac9e2de357e66bf04acf497ff
Uwa Joseph Uwa <uwauwajoe@gmail.com>
2024-02-23 00:58:20 -0700

    errors corrected on week 2 task

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,46 +3,31 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullName = "Uwa Joseph Uwa";
-const currentYear = "2024";
-const profilePicture = "images/uwajoe.jpg";
-
+let FullName = "Uwa Joseph Uwa";
+let currentYear = "2024";
+let profilePicture = "images/uwajoe.jpg";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("name");
-const foodElement = document.getElementById("food");
-const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("img");
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullName}<strong>`;
+nameElement.innerHTML = `<strong>${FullName}<strong>`;
 yearElement.textContent = currentYear;
-imageElement.setAttribute("src", profilePicture);
-imageElement.setAttribute("alt", `profile picture of ${profilePicture}`);
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `profile image of ${profilePicture}`);
 
 
 /* Step 5 - Array */
-let favFood = ["Beans", "White Soup", "Pizza", "Afang Soup", "Fried Rice", "Egusi Soup", "Banana", "Ice cream", "Strawberry"];
-let singlefavfood = "Watermelon";
+let favFood = ["White Soup", "Pizza", "Afang Soup", "Fried Rice", "Egusi Soup", "Banana", "Ice cream", "Strawberry"];
+let singlefavFood = "African Salad";
 foodElement.textContent = favFood;
 favFood.push(singlefavFood);
 foodElement.innerHTML += `<br>${favFood}`;
-favFood.splice(0, 1);
+favFood.splice(0,1);
 foodElement.innerHTML += `<br>${favFood}`;
 favFood.pop();
 foodElement.innerHTML += `<br>${favFood}`;
-
-// // Add a new food to the array
-// let newfood = "Watermelon";
-// favFood.push(newfood);
-// foodElement.innerHTML += `<br>${newfood}`;
-
-// // Remove the first and last elements
-// const firstFood = favFood.shift();
-// const lastFood = favFood.pop();
-
-// // Render the removed elements in addition to the previous array values (with a line break)
-// foodElement.innerHTML += `<br>${firstFood}`;
-// foodElement.innerHTML += `<br>${lastFood}`;
